c894aa7a529f7bced0ad94ff6d5be2972190c8f8
Kaitwinner <Kaitevander@gmail.com>
2025-03-17 14:06:06 +0200

    Add files via upload

---
+++ pariselt-seekord/player.gd
@@ -0,0 +1,29 @@
+class_name Player extends CharacterBody2D
+
+var _this_scene = preload("res://Player.tscn")
+
+func new() -> Player:
+	return _this_scene.instantiate()
+
+@export var speed = 400
+@export var gravity = 980
+@export var jump_force = -500 
+
+func _get_input():
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jump_force
+	if Input.is_action_just_pressed("shoot"):
+		_shoot()
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	velocity *= 0.99
+	
+	_get_input()
+	move_and_slide()
+
+func _shoot():
+	var angle_vec = (get_global_mouse_position() - global_position).normalized()
+	
+	velocity = -angle_vec * 1000.0
